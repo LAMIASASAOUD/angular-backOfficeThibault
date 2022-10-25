06a9a26bad5398e1bdd725f5d9151495d543677e
AIT EL BACHA <r.aitelbacha@gmail.com>
2022-10-25 12:09:30 +0200

    navigation / component erreur / route-pere-fills

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ErrComponent } from './err/err.component';
+import { ProduitComponent } from './produit/produit.component';
 
-const routes: Routes = [];
+const routes: Routes = [  
+  {path:'',component:AppComponent },
+
+  {path:'admin',children:[
+    //les garde canActivate:[MygardsGuard]  pour  activer  que a les admin  pour vesiter  cette  page  
+    {path:'produit', component:ProduitComponent},  
+  
+  ]},
+  
+  // si l url pas vrais  fout afficher  cette  conponent 
+  
+  {path:'**',component:ErrComponent}, 
+  
+  
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { ProduitComponent } from './produit/produit.component';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatButtonModule} from '@angular/material/button';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatTableModule} from '@angular/material/table';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ErrComponent } from './err/err.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatMenuModule} from '@angular/material/menu';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProduitComponent,
+    NavbarComponent,
+    ErrComponent
   ],
   imports: [
     BrowserModule,
+    MatCardModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,MatMenuModule,
+    MatSidenavModule,MatButtonModule,MatButtonToggleModule,MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
